7efcabbb2540added47287aab6952a6412561912
LoudenA <aloude6942@greenvilleschools.us>
2017-05-17 14:43:31 -0400

    Add files via upload

---
+++ geometry.js
@@ -0,0 +1,41 @@
+function rectangleArea(b, h)
+{
+    var a = b * h;
+
+    return a;
+}
+
+function rectanglePerimeter(b, h)
+{
+    var p = 2 * b + 2 * h;
+
+    return p;
+}
+
+function triangleArea(b, h)
+{
+    var a = b * h / 2;
+
+    return a;
+}
+
+function trianglePerimeter(b, h)
+{
+    var p = 2 * b + 2 * h;
+
+    return p;
+}
+
+function trapezoidArea(a, b, h )
+{
+    var a = a + b / 2 * h;
+
+    return a;
+}
+
+function rectangularprismVolume(a, h, l)
+{
+  var v = a*h*l
+
+  return v;
+}
